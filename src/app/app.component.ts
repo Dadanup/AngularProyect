4db315d85f9347fb83da48a665ab905121fdef65
Jesús Daniel Bautista Banda <85539850+Dadanup@users.noreply.github.com>
2021-09-09 17:43:31 -0500

    VERSION UNO
    Falta implementar validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularProyect';
+  
+  title = 'BIENVENIDO';
+  sentence = 'Ingresa los siguientes datos';
 }
