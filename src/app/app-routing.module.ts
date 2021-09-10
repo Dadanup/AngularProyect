2928af381e3d87712b52116a199f073386ccd0c9
Jesús Daniel Bautista Banda <85539850+Dadanup@users.noreply.github.com>
2021-09-10 11:32:09 -0500

    VERSION TRES
    Se elimino el componente button-validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
-import {ButtonValidationComponent} from './form/button-validation/button-validation.component'
 const routes: Routes = [
-  {path:'form',component: FormComponent},
-  {path:'button-validation', component:ButtonValidationComponent }
+  {path:'form',component: FormComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
-import { ButtonValidationComponent } from './form/button-validation/button-validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-    ButtonValidationComponent,
+    FormComponent
     
   ],
   imports: [
